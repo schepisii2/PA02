ad31012d86a0276d461216478f8c8ff299acbb90
Isabella Schepisi <schepisii2@winthrop.edu>
2020-03-06 01:00:56 -0500

    first commit

--- WordFind.java
+++ WordFind.java
@@ -1,171 +1,198 @@
-/*
- * Isabella Schepisi
- * schepisii2@winthrop.edu
- *
- * CSCI 271 001
- * PA02 - WordFind
- *
- **/
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.*;
 
 public class WordFind
 {
-	public static boolean north(char[][] myArray, String target, int rowPos, int colPos)
+	public static void printArray(char[][] arr, int r, int c)
 	{
-		if (target.length()>rowPos)
+		for (int i=0;i<r;i++)
+		{
+			for (int j=0;j<c;j++)
+				System.out.print(arr[i][j]);
+			System.out.println(" ");
+		}
+	}
+	public static boolean north(char[][] arr, String target, int r, int c)
+	{
+		if (r<target.length()-1)
 			return false;
-		for (int i=rowPos;i<rowPos-target.length();i--)
-		{
-			if (myArray[i][colPos]!=target.charAt(i-rowPos))
-				return false;
-		}
-		return true;
-	}
-
-	public static boolean east(char[][] myArray, String target, int rowPos, int colPos)
-	{
-		for (int i=colPos;i<colPos+target.length();i++)
-		{
-			if (myArray[rowPos][i]!=target.charAt(i-colPos))
-				return false;
-		}
-		return true;
-	}
-	public static boolean south(char[][] myArray, String target, int rowPos, int colPos)
-	{
-		for (int i=rowPos;i<rowPos+target.length();i++)
-		{
-			if (myArray[i][colPos]!=target.charAt(i-rowPos))
-				return false;
-		}
-		return true;
-	}
-	public static boolean west(char[][] myArray, String target, int rowPos, int colPos)
-	{
-		for (int i=colPos;i<colPos-target.length();i--)
-		{
-			if (myArray[i][colPos]!=target.charAt(i-rowPos))
-				return false;
-		}
-		return true;
-	}
-	public static boolean northeast(char[][] myArray, String target, int rowPos, int colPos)
-	{
 		for (int i=0;i<target.length();i++)
-		{
-			if (myArray[rowPos-i][colPos+i]!=target.charAt(i))
+			if(arr[r-i][c]!=target.charAt(i))
 				return false;
-		}
 		return true;
 	}
-	public static boolean northwest(char[][] myArray, String target, int rowPos, int colPos)
+	public static boolean south(char[][] arr, String target, int r, int c)
 	{
+		if (15-r<target.length()-1)
+			return false;
 		for (int i=0;i<target.length();i++)
-		{
-			if (myArray[rowPos-i][colPos-i]!=target.charAt(i))
+			if(arr[r+i][c]!=target.charAt(i))
 				return false;
-		}
 		return true;
 	}
-	public static boolean southeast(char[][] myArray, String target, int rowPos, int colPos)
+	public static boolean east(char[][] arr, String target, int r, int c)
 	{
+		if (15-c<target.length()-1)
+			return false;
 		for (int i=0;i<target.length();i++)
-		{
-			if (myArray[rowPos+i][colPos+i]!=target.charAt(i))
+			if(arr[r][c+i]!=target.charAt(i))
 				return false;
-		}
 		return true;
 	}
-	public static boolean southwest(char[][] myArray, String target, int rowPos, int colPos)
+	public static boolean west(char[][] arr, String target, int r, int c)
 	{
+		if (c<target.length()-1)
+			return false;
 		for (int i=0;i<target.length();i++)
-		{
-			if (myArray[rowPos+i][colPos-i]!=target.charAt(i))
+			if(arr[r][c-i]!=target.charAt(i))
 				return false;
-		}
 		return true;
 	}
-	public static String checkDirection(char[][] myArray, String target, int rowPos, int colPos)
+	public static boolean northeast(char[][] arr, String target, int r, int c)
 	{
-		if (north(myArray,target,rowPos,colPos))
-			return "n";
-/*		else if (east(myArray,target,rowPos,colPos))
-			return "e";
-		else if (south(myArray,target,rowPos,colPos))
-			return "s";
-		else if (west(myArray,target,rowPos,colPos))
-			return "w";
-		else if (northeast(myArray,target,rowPos,colPos))
-			return "ne";
-		else if (northwest(myArray,target,rowPos,colPos))
-			return "nw";
-		else if (southeast(myArray,target,rowPos,colPos))
-			return "se";
-		else if (southwest(myArray,target,rowPos,colPos))
-			return "sw"; */
-		else
-			return "default";
-
+		if ((r<target.length()-1)||(15-c<target.length()-1))
+			return false;
+		for (int i=0;i<target.length();i++)
+			if(arr[r-i][c+i]!=target.charAt(i))
+				return false;
+		return true;
 	}
-
-	public static void main(String[] args) throws IOException{
-		File f=new File("cashiers.txt");     //Creation of File Descriptor for input file
-		FileReader fr=new FileReader(f);   //Creation of File Reader object
-	        BufferedReader br=new BufferedReader(fr);  //Creation of BufferedReader object
-		
-		int b = 0;             
-		
-		int rowCount=0, columnCount=0, rows=15, columns=15;
-		char[][] myArray = new char[rows][columns];
-
-		while((b = br.read()) != -1)         //Read char by Char
-		{
-			char character = (char) b;
-			if ((character!='|')&&(character!='-')&&(character!='\n'))
+	public static boolean northwest(char[][] arr, String target, int r, int c)
+	{
+		if ((r<target.length()-1)||(c<target.length()-1))
+			return false;
+		for (int i=0;i<target.length();i++)
+			if(arr[r-i][c-i]!=target.charAt(i))
+				return false;
+		return true;
+	}
+	public static boolean southeast(char[][] arr, String target, int r, int c)
+	{
+		if ((15-r<target.length())||(15-c<target.length()-1))
+			return false;
+		for (int i=0;i<target.length();i++)
+			if(arr[r+i][c+i]!=target.charAt(i))
+					return false;
+		return true;
+	}
+	public static boolean southwest(char[][] arr, String target, int r, int c)
+	{
+		if ((15-r<target.length()-1)||(c<target.length()-1))
+			return false;
+		for (int i=0;i<target.length();i++)
+			if(arr[r+i][c-i]!=target.charAt(i))
+				return false;
+		return true;
+	}
+	public static void findWord(char[][] myArray, String myWord, int rows, int columns)
+	{
+		boolean isFound;
+		for (int i=0;i<rows;i++)
+	        {
+	                for (int j=0;j<columns;j++)
 			{
-				//System.out.println(character);
-				myArray[rowCount][columnCount]=character;
-				columnCount++;
-				//System.out.println("column" + columnCount);
-				if (columnCount==15)
-				{
-					columnCount=0;
-					rowCount++;
-					//System.out.println("row" + rowCount);
+				boolean isNorth = north(myArray,myWord,i,j);
+	                        boolean isSouth = south(myArray,myWord,i,j);
+			        boolean isEast = east(myArray,myWord,i,j);
+			        boolean isWest = west(myArray,myWord,i,j);
+			        boolean isNortheast = northeast(myArray,myWord,i,j);
+			        boolean isNorthwest = northwest(myArray,myWord,i,j);
+			        boolean isSoutheast = southeast(myArray,myWord,i,j);
+			        boolean isSouthwest = southwest(myArray,myWord,i,j);
+			        if(WordFind.isFound = (isNorth||isSouth||isEast||isWest||isNortheast||isNorthwest||isSoutheast||isSouthwest))
+			    	{
+					if (isNorth)
+				        	System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented North");      
+			                if (isSouth)
+			                        System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented South");      
+			                if (isEast)
+			                        System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented East");       
+					if (isWest)
+                                                System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented West");       
+					if (isNortheast)
+						System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Northeast");
+					if (isNorthwest)						 
+				                System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Northwest");  
+					if (isSoutheast)					
+			                        System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Southeast");  
+					if (isSouthwest)						 
+                                                System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Southwest");  
+					break;					 
 				}
 			}
 		}
-	
-		String myWord="ANTIQUE", direction="default";
-
-		for (int r=0;r<rows;r++)
-		{
-			for (int c=0;c<columns;c++)
-			{
-				direction=checkDirection(myArray,myWord,r,c);
-			}
-		}
-
-/*		String n = "ANTIQUE";
-		System.out.println(north(myArray,n,14,4));
-		String e = "BASS";
-		System.out.println(east(myArray,e,3,1));	
-		String s = "BOAT";
-		System.out.println(south(myArray, s, 5, 11));
-		String w = "LAKE";
-		System.out.println(west(myArray,w,13,3));
-		String ne = "CROQUET";
-		System.out.println(northeast(myArray,ne,14,8));
-		String nw = "JELLY";
-		System.out.println(northwest(myArray,nw,13,14));
-		String se = "LODGE";
-		System.out.println(southeast(myArray,se,9,6));
-		String sw = "DOWNTOWN"; //5,12
-		System.out.println(southwest(myArray,sw,5,12)); */
+		/* This code is supposed to indicate when a word is not found.
+		 * It does not work because there are scope issues with the variable isFound
+		 *
+		 *
+		 * if (!isFound)
+			System.out.println(myWord + " was not found.");*/
 	}
 
+	public static void main(String[] args) throws IOException
+	{
+		//declare variables
+		File f=new File(/*"cashiers.txt"*/args[0]);
+		FileReader fr=new FileReader(f);
+		BufferedReader br=new BufferedReader(fr);
+		int c=0;
+		/* 
+		 * The variables rows and columns are predetermined to be 15,
+		 * but they should be dependent on the number of rows/columns in arg[0]
+		 */
+		int rowCount=0,columnCount=0,rows=15,columns=15;
+		char[][] myArray = new char[rows][columns];
+		String myWord = new String();
+		//read arg[0] file
+		while((c=br.read()) != -1)
+		{
+			char character = (char) c;
+			//put char in 2d array
+			if ((character!='-')&&(character!='|')&&(character!='\n'))
+			{
+				myArray[rowCount][columnCount]=character;
+				columnCount++;
+
+				if (columnCount==columns)
+				{
+					columnCount=0;
+					rowCount++;
+				}
+			}	
+		}
+		//print array
+		printArray(myArray,rows,columns);
+		/* if there is not a second argument,
+		 * prompt user for word to find */
+		if (args.length==1)
+		{
+			Scanner myObj = new Scanner(System.in);
+			System.out.println("Enter word:");
+			myWord=myObj.nextLine();
+			do{
+				findWord(myArray,myWord,rows,columns);
+				System.out.println("Enter word:");
+				myWord=myObj.nextLine();
+			}while(myWord.length()!=0);
+		}
+		/* if there is a second argument,
+		 * read in words from file */
+		if (args.length==2)
+		{
+			File myFile = new File(/*cashwords.txt*/args[1]);
+			Scanner myReader = new Scanner(myFile);
+			while (myReader.hasNextLine())
+			{
+				String word = myReader.nextLine();
+				findWord(myArray,word,rows,columns);
+
+			}
+			myReader.close();
+		}
+
+	}
 }
