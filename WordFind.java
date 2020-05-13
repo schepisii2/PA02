322f52c0a6f714556775c5cb1b597976223724b9
Isabella Schepisi <56453624+schepisii2@users.noreply.github.com>
2020-05-13 16:47:02 -0400

    Update directions and variables
    Update direction functions to account for an adjustable word_grid by using rows/columns variable.
    Update variable names to be more concise.

--- WordFind.java
+++ WordFind.java
@@ -4,10 +4,8 @@
  * CSCI 271 001
  */
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.lang.*;
 
 public class WordFind
 {
@@ -29,18 +27,18 @@ public class WordFind
 				return false;
 		return true;
 	}
-	public static boolean south(char[][] arr, String target, int r, int c)
+	public static boolean south(char[][] arr, String target, int r, int c, int rows)
 	{
-		if (15-r<target.length()-1)
+		if (rows-r<target.length()-1)
 			return false;
 		for (int i=0;i<target.length();i++)
 			if(arr[r+i][c]!=target.charAt(i))
 				return false;
 		return true;
 	}
-	public static boolean east(char[][] arr, String target, int r, int c)
+	public static boolean east(char[][] arr, String target, int r, int c, int columns)
 	{
-		if (15-c<target.length()-1)
+		if (columns-c<target.length()-1)
 			return false;
 		for (int i=0;i<target.length();i++)
 			if(arr[r][c+i]!=target.charAt(i))
@@ -56,9 +54,9 @@ public class WordFind
 				return false;
 		return true;
 	}
-	public static boolean northeast(char[][] arr, String target, int r, int c)
+	public static boolean northeast(char[][] arr, String target, int r, int c, int columns)
 	{
-		if ((r<target.length()-1)||(15-c<target.length()-1))
+		if ((r<target.length()-1)||(columns-c<target.length()-1))
 			return false;
 		for (int i=0;i<target.length();i++)
 			if(arr[r-i][c+i]!=target.charAt(i))
@@ -74,65 +72,65 @@ public class WordFind
 				return false;
 		return true;
 	}
-	public static boolean southeast(char[][] arr, String target, int r, int c)
+	public static boolean southeast(char[][] arr, String target, int r, int c, int rows, int columns)
 	{
-		if ((15-r<target.length())||(15-c<target.length()-1))
+		if ((rows-r<target.length())||(columns-c<target.length()-1))
 			return false;
 		for (int i=0;i<target.length();i++)
 			if(arr[r+i][c+i]!=target.charAt(i))
 					return false;
 		return true;
 	}
-	public static boolean southwest(char[][] arr, String target, int r, int c)
+	public static boolean southwest(char[][] arr, String target, int r, int c, int rows)
 	{
-		if ((15-r<target.length()-1)||(c<target.length()-1))
+		if ((rows-r<target.length()-1)||(c<target.length()-1))
 			return false;
 		for (int i=0;i<target.length();i++)
 			if(arr[r+i][c-i]!=target.charAt(i))
 				return false;
 		return true;
 	}
-	public static void findWord(char[][] myArray, String myWord, int rows, int columns)
+	public static void findWord(char[][] word_grid, String word, int rows, int columns)
 	{
-		String tempWord = myWord.replaceAll(" ","");
+		String temp_word = word.replaceAll(" ","").toUpperCase();
 		boolean isFound=false;
 		for (int i=0;i<rows;i++)
 	        {
-	                for (int j=0;j<columns;j++)
-			{
-				boolean isNorth = north(myArray,tempWord,i,j);
-	         		boolean isSouth = south(myArray,tempWord,i,j);
-			        boolean isEast = east(myArray,tempWord,i,j);
-			        boolean isWest = west(myArray,tempWord,i,j);
-			        boolean isNortheast = northeast(myArray,tempWord,i,j);
-			        boolean isNorthwest = northwest(myArray,tempWord,i,j);
-			        boolean isSoutheast = southeast(myArray,tempWord,i,j);
-			        boolean isSouthwest = southwest(myArray,tempWord,i,j);
+	            for (int j=0;j<columns;j++)
+				{
+					boolean isNorth = north(word_grid,temp_word,i,j);
+	         		boolean isSouth = south(word_grid,temp_word,i,j,rows);
+			    	boolean isEast = east(word_grid,temp_word,i,j,columns);
+			        boolean isWest = west(word_grid,temp_word,i,j);
+			        boolean isNortheast = northeast(word_grid,temp_word,i,j,columns);
+			        boolean isNorthwest = northwest(word_grid,temp_word,i,j);
+			        boolean isSoutheast = southeast(word_grid,temp_word,i,j,rows,columns);
+			        boolean isSouthwest = southwest(word_grid,temp_word,i,j,rows);
 			        isFound = (isNorth||isSouth||isEast||isWest||isNortheast||isNorthwest||isSoutheast||isSouthwest);
 				if (isFound)
 			    	{
 					if (isNorth)
-				        	System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented North");      
-			                if (isSouth)
-			                        System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented South");      
-			                if (isEast)
-			                        System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented East");       
+				        System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented North");      
+			        if (isSouth)
+			            System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented South");      
+			    	if (isEast)
+			            System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented East");       
 					if (isWest)
-                                                System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented West");       
+                        System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented West");       
 					if (isNortheast)
-						System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Northeast");
+						System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Northeast");
 					if (isNorthwest)						 
-				                System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Northwest");  
+				        System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Northwest");  
 					if (isSoutheast)					
-			                        System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Southeast");  
+			            System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Southeast");  
 					if (isSouthwest)						 
-                                                System.out.println(myWord + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Southwest");  
+                        System.out.println(word + " was found starting at " + (i+1) + "," + (j+1) + " and oriented Southwest");  
 					return;					 
 				}
 			}
 		}
 		if (!isFound)
-			System.out.println(myWord + " was not found.");
+			System.out.println(word + " was not found.");
 	}
 
 	public static void main(String[] args) throws IOException
@@ -144,75 +142,75 @@ public class WordFind
 			System.out.println("ERROR: no input file specified.");
 			System.exit(1);
 		}
-		File f=new File(/*"cashiers.txt"*/args[0]);
-		/* checks that the input file exists
+		File grid_file=new File(args[0]);
+		/* checks that the word grid file exists
 		 * if not, the program quits */
-		if(!(f.exists()))
+		if(!(grid_file.exists()))
 		{
-			System.out.println("ERROR: could not open input file.");
+			System.out.println("ERROR: could not open word grid file.");
 			System.exit(2);
 		}
 		/* read file
 		 * record number of rows and columns */
 		int rows=0,columns=0;
-		Scanner s1 = new Scanner(f);
-		while (s1.hasNextLine())
+		Scanner scan_dimension = new Scanner(grid_file);
+		String grid_line = new String();
+		while (scan_dimension.hasNextLine())
 		{
-			String temp1 = new String();
-			temp1=s1.nextLine();
-			columns=temp1.length();
+			
+			grid_line=scan_dimension.nextLine().replaceAll("-", "").replace("|","");
+			if (!(grid_line.length()==0)){
+				columns=grid_line.length();
+			}
 			rows++;
 		}
-		s1.close();
+		scan_dimension.close();
 		rows=(rows-1)/2;
-		columns=(columns-1)/2;
 		/* read file again
 		 * record data into 2d array */
-		char[][] myArray = new char[rows][columns];
+		char[][] word_grid = new char[rows][columns];
 		int r=0;
-		Scanner s2 = new Scanner(f);
-		while (s2.hasNextLine())
+		Scanner scan_grid = new Scanner(grid_file);
+		while (scan_grid.hasNextLine())
 		{
-			String temp2 = new String();
-			temp2=s2.nextLine().replace("-","").replace("|","");
-			if (!(temp2.length()==0))
+			grid_line=scan_grid.nextLine().replace("-","").replace("|","");
+			if (!(grid_line.length()==0))
 			{
 				for (int c=0;c<columns;c++)
 				{
-					myArray[r][c]=temp2.charAt(c);
+					word_grid[r][c]=grid_line.charAt(c);
 				}
 				r++;
 			}	
 		}
-		s2.close();
+		scan_grid.close();
 		/* if there is not a second argument,
 		 * prompt user for word to find */
-		String myWord = new String();
+		String word = new String();
 		if (args.length==1)
 		{
-			Scanner myObj = new Scanner(System.in);
+			Scanner scan_input = new Scanner(System.in);
 			System.out.print("Enter word: ");
-			myWord=myObj.nextLine().toUpperCase();
+			word=scan_input.nextLine();
 			do{
-				findWord(myArray,myWord,rows,columns);
+				findWord(word_grid,word,rows,columns);
 				System.out.print("Enter word: ");
-				myWord=myObj.nextLine().replaceAll(" ","").toUpperCase();
-			}while(myWord.length()!=0);
-			myObj.close();
+				word=scan_input.nextLine();
+			}while(word.length()!=0);
+			scan_input.close();
 		}
 		/* if there is a second argument,
 		 * read in words from file */
 		if (args.length==2)
 		{
-			File myFile = new File(/*cashwords.txt*/args[1]);
-			Scanner myReader = new Scanner(myFile);
-			while (myReader.hasNextLine())
+			File word_file = new File(/*word file*/args[1]);
+			Scanner scan_word = new Scanner(word_file);
+			while (scan_word.hasNextLine())
 			{
-				String word = myReader.nextLine().replaceAll(" ","").toUpperCase();
-				findWord(myArray,word,rows,columns);
-
+				word = scan_word.nextLine();
+				findWord(word_grid,word,rows,columns);
 			}
-			myReader.close();
+			scan_word.close();
 		}
 
 	}
